84c85254f854aa4cea390e8beb26f65e985959fc
Ganyarat Somjak <fon_gunyarat@hotmail.com>
2015-09-11 10:35:54 +0700

    new switch File

---
+++ switch.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+      int s;
+      scanf("%d",&s);
+      
+      switch(s)
+      {
+              case 1:
+              printf("ONE");
+              break;
+              case 2:
+              printf("\nTWO");
+              break;
+              default:
+              printf("\nNo");        
+              break;
+              
+      }
+      
+      getch();
+      
+}
